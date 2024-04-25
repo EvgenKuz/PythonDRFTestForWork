37236e22f914d9cc36ae469eec79c890f15d96b1
Евгений Кузнецов <eugeny.kuz@gmail.com>
2024-04-26 02:03:00 +0500

    Added functionality to copy files that are being manipulated.

--- apps/video/tests/tasks/test_change_resolution_of_video.py
+++ apps/video/tests/tasks/test_change_resolution_of_video.py
@@ -1,8 +1,10 @@
+import glob
 import os
 import uuid
 from unittest.mock import Mock, patch
 
 import ffmpeg
+from django.conf import settings
 from django.test import TestCase
 
 from apps.video.models import Video
@@ -16,6 +18,12 @@ class ChangeResolutionOfVideoTaskTests(TestCase):
 
     def tearDown(self):
         for video in self.created_videos:
+            video_id = video.split("/")[-1][:-4]
+            videos_root = settings.MEDIA_ROOT / "videos"
+            copies = glob.glob1(videos_root, f"{video_id}_*")
+
+            for copy in copies:
+                os.remove(videos_root / copy)
             os.remove(video)
 
     def test_task_change_resolution_of_video(self):
@@ -33,6 +41,9 @@ class ChangeResolutionOfVideoTaskTests(TestCase):
         video.refresh_from_db()
         self.assertTrue(video.last_processed_success)
         self.assertFalse(video.is_processing)
+        self.assertEqual(
+            len(glob.glob1(settings.MEDIA_ROOT / "videos", f"{video.id}*")), 2
+        )
 
     @patch("apps.video.models.Video.refresh_from_db")
     @patch("apps.video.services.VideoService.change_video_resolution")
@@ -70,5 +81,19 @@ class ChangeResolutionOfVideoTaskTests(TestCase):
         change_resolution_of_video(video.id, width, height)
 
         video.refresh_from_db()
+        self.assertFalse(video.last_processed_success)
+        self.assertFalse(video.is_processing)
+
+    @patch("apps.video.services.VideoService.make_copy_of_video")
+    def test_failed_to_make_copy(self, make_video_copy: Mock):
+        video = create_video()
+        self.created_videos.append(video.file.path)
+        self.assertIsNone(video.last_processed_success)
+
+        make_video_copy.side_effect = IOError()
+        width = 500
+        height = 500
+
+        change_resolution_of_video(video.id, width, height)
         self.assertFalse(video.last_processed_success)
         self.assertFalse(video.is_processing)
