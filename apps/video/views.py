852cf543a2a2afea69496d621bf097ec79881052
Eugeniy Kuznetsov <55546545+EvgenKuz@users.noreply.github.com>
2024-04-26 01:33:45 +0500

    Added functionality to get video's info and status
    * Added functionality to get video's info and status
    
    * Fixed nginx config

--- apps/video/views.py
+++ apps/video/views.py
@@ -9,11 +9,25 @@ from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 
 from apps.video.models import Video
-from apps.video.serializers import UploadVideo, VideoIdSerializer, VideoResolution
+from apps.video.serializers import (
+    UploadVideo,
+    VideoIdSerializer,
+    VideoResolution,
+    VideoStatusSerializer,
+)
 from apps.video.services import ErrorMessageService
 from apps.video.tasks import change_resolution_of_video
 
 logger = logging.getLogger(__name__)
+
+
+OPENAPI_ID_PARAM_SCHEME = openapi.Parameter(
+    name="id",
+    description="Video's id",
+    type=openapi.TYPE_STRING,
+    in_=openapi.IN_PATH,
+    format=openapi.FORMAT_UUID,
+)
 
 
 class VideoUploadAPIView(views.APIView):
@@ -55,15 +69,7 @@ class VideoUploadAPIView(views.APIView):
 class VideoAPIView(views.APIView):
     @swagger_auto_schema(
         operation_description="Delete video file",
-        manual_parameters=[
-            openapi.Parameter(
-                name="id",
-                description="Video's id",
-                type=openapi.TYPE_STRING,
-                in_=openapi.IN_PATH,
-                format=openapi.FORMAT_UUID,
-            )
-        ],
+        manual_parameters=[OPENAPI_ID_PARAM_SCHEME],
         responses={
             200: openapi.Response(
                 description="Video deletion attempted",
@@ -96,15 +102,7 @@ class VideoAPIView(views.APIView):
 
     @swagger_auto_schema(
         operation_description="Change video's resolution.",
-        manual_parameters=[
-            openapi.Parameter(
-                name="id",
-                description="Video's id",
-                type=openapi.TYPE_STRING,
-                in_=openapi.IN_PATH,
-                format=openapi.FORMAT_UUID,
-            )
-        ],
+        manual_parameters=[OPENAPI_ID_PARAM_SCHEME],
         request_body=VideoResolution,
         responses={
             200: openapi.Response(
@@ -152,3 +150,26 @@ class VideoAPIView(views.APIView):
             return Response(status=status.HTTP_200_OK, data={"success": False})
 
         return Response(status=status.HTTP_200_OK, data={"success": True})
+
+    @swagger_auto_schema(
+        operation_description="Get video's information and processing status.",
+        manual_parameters=[OPENAPI_ID_PARAM_SCHEME],
+        responses={
+            200: openapi.Response(
+                description="Video's info and status.", schema=VideoStatusSerializer
+            ),
+            400: "Incorrect id sent",
+        },
+    )
+    def get(self, request: views.Request, id: uuid.UUID):
+        try:
+            video = Video.objects.get(id=id)
+        except Video.DoesNotExist:
+            logger.warning("Incorrect id sent.")
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST, data={"error": "Incorrect id sent."}
+            )
+
+        data = VideoStatusSerializer(instance=video).data
+
+        return Response(status=status.HTTP_200_OK, data=data)
